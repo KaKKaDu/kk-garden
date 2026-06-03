dbe36def3a5604e9fe23496f7ad3747a85e3cc34
Kiryl Kartsialeu <k.kortelev@itransition.com>
2026-06-04 00:16:05 +0200

    v1.0.2: added the errors types and utils

---
+++ src/errors/app-error.ts
@@ -0,0 +1,29 @@
+import type {DataObject} from "./success-data";
+
+export type AppErrorConfig = {
+  message: string;
+  status?: number;
+  context?: DataObject;
+  error?: Error;
+};
+
+export class AppError extends Error {
+  status?: number;
+  context?: DataObject;
+  error?: Error;
+
+  get isServer(): boolean {
+    return !!(this.status && this.status > 500 && this.status < 600);
+  }
+
+  get isClient(): boolean {
+    return !!(this.status && this.status >= 400 && this.status < 500);
+  }
+
+  constructor({ status, message, context, error }: AppErrorConfig) {
+    super(message);
+    this.status = status;
+    this.context = context;
+    this.error = error;
+  }
+}
---
+++ src/errors/handle-error.ts
@@ -0,0 +1,30 @@
+import type {DataObject, SuccessData} from "./success-data";
+import {AppError} from "./app-error";
+
+export const handleError = <Data extends DataObject = DataObject>(e: unknown): SuccessData<false, Data> => {
+  let statusCode: number = 0;
+  let message: string = 'Unknown error';
+  if (e instanceof Error) {
+    message = `${e.message}`;
+    return { success: false, errors: [new AppError({ message, status: 0, context: JSON.stringify(e) })] };
+  }
+  if (e && typeof e === 'object') {
+    if ('code' in e) {
+      statusCode = parseInt(e.code as string);
+    } else if ('status' in e) {
+      statusCode = parseInt(e.status as string);
+    } else if ('statusCode' in e) {
+      statusCode = parseInt(e.statusCode as string);
+    }
+    if ('data' in e) {
+      message = JSON.stringify(e.data);
+    } else if ('description' in e) {
+      message = JSON.stringify(e.description);
+    } else {
+      message = JSON.stringify(e);
+    }
+  } else if (e && typeof e === 'string') {
+    message = e;
+  }
+  return { success: false, errors: [new AppError({ message, status: statusCode, context: JSON.stringify(e) })] };
+};
