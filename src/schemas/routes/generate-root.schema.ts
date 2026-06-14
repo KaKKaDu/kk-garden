e8faf4b605b9495fea7dd4217712dcb84d011c75
Kiryl Kartsialeu <k.kortelev@itransition.com>
2026-06-14 17:51:05 +0200

    generate root schemas updates

--- src/schemas/routes/generate-root.schema.ts
+++ src/schemas/routes/generate-root.schema.ts
@@ -4,15 +4,15 @@ import { SpritesheetSetDtoSchema } from '../models/spritesheet-set-dto.schema';
 import { AnimatedSpriteDtoSchema } from '../models/animated-sprite-dto.schema';
 
 export const GenerateRootResponseSchema = z.object({
-  staticData: {
+  staticData: z.object({
     tiles: z.array(GridDrawerElementWithIdSchema),
     stones: z.array(GridDrawerElementWithIdSchema),
-  },
-  animatedData: {
+  }),
+  animatedData: z.object({
     spritesheetSets: z.record(z.string(), SpritesheetSetDtoSchema),
     trees: z.array(AnimatedSpriteDtoSchema),
     bushes: z.array(AnimatedSpriteDtoSchema),
-  },
+  }),
 });
 
 export const GenerateRootQueryStringSchema =  z.object({
