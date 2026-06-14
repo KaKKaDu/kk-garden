55678f7f1326ee9a7d3e672185ae7e0fd2952827
Kiryl Kartsialeu <k.kortelev@itransition.com>
2026-06-14 14:08:46 +0200

    index file update

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -14,4 +14,4 @@ export { SpriteDataSchema } from './models/sprite-data.schema';
 export { SpritesheetPathSchema } from './models/spritesheet-path.schema';
 export { SpritesheetElementSchema } from './models/spritesheet-element.schema';
 export { SpritesheetSetDtoSchema } from './models/spritesheet-set-dto.schema';
-export { GenerateRootResponseSchema } from './routes/generate-root.schema';
+export { GenerateRootResponseSchema, GenerateRootQueryStringSchema } from './routes/generate-root.schema';
